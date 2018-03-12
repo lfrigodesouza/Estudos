bfa824cadea2a9199c95e19504bd643c3a5a2af7
Lucas Frigo de Souza <lfrigodesouza@outlook.com>
2018-03-12 20:59:43 -0300

    Delegates e Eventos

---
+++ ByteBank.Agencias/ByteBank.Agencias/EdicaoAgencia.xaml.cs
@@ -0,0 +1,71 @@
+﻿using ByteBank.Agencias.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ByteBank.Agencias
+{
+    /// <summary>
+    /// Interaction logic for EdicaoAgencia.xaml
+    /// </summary>
+    public partial class EdicaoAgencia : Window
+    {
+        private readonly Agencia _agencia;
+        public EdicaoAgencia(Agencia agencia)
+        {
+            InitializeComponent();
+            _agencia = agencia ?? throw new ArgumentNullException(nameof(agencia));
+            AtualizarCamposDeTexto();
+            AtualizarControles();
+        }
+
+        private void AtualizarCamposDeTexto()
+        {
+            txtNumero.Text = _agencia.Numero;
+            txtNome.Text = _agencia.Nome;
+            txtTelefone.Text = _agencia.Telefone;
+            txtEndereco.Text = _agencia.Endereco;
+            txtDescricao.Text = _agencia.Descricao;
+        }
+
+        private void AtualizarControles()
+        {
+            RoutedEventHandler dialogResultTrue = (o, e) => DialogResult = true;
+            RoutedEventHandler dialogResultFalse = (o, e) => DialogResult = false;
+
+            var okEventHandler = dialogResultTrue + Fechar;
+            var cancelarEventHandler = dialogResultFalse + Fechar;
+
+            btnOk.Click += cancelarEventHandler;
+            btnCancelar.Click += okEventHandler;
+
+            txtNome.TextChanged += TxtNome_TextChanged;
+        }
+
+        private void TxtNome_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textoEstaVazio = String.IsNullOrWhiteSpace(txtNome.Text);
+            if (textoEstaVazio)
+            {
+                txtNome.Background = new SolidColorBrush(Colors.OrangeRed);
+            }
+            else
+            {
+                txtNome.Background = new SolidColorBrush(Colors.White);
+            }
+        }
+
+        private void Fechar(object sender, EventArgs e) =>
+            Close();
+    }
+}
